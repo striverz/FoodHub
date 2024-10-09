b858c8cc72eacc25a757fa06b3a0f924761ee029
striverz <striverz100x@gmail.com>
2024-10-09 23:09:23 +0530

    updated the footer and navbar is responsiveness

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./Footer.css"
+import logo from "../../assets/logo.png"
 
 const Footer = () => {
   return (
     <div className="footer">
-        <h4>MANIKANTA KORIMILLI</h4>
+        
+        <div className="footer-left">
+          <img src={logo} className="footer-logo"></img>
+          <h4>FoodHub</h4>
+        </div>
+        <div className="footer-right">
+          <h5>MANIKANTA KORIMILLI</h5>
+          <p>Fullstack Developer | Competitive Programmer</p>
+        </div>
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,45 +1,53 @@
-import React from "react";
-import "./Navbar.css"
-import logo from "../../assets/logo.png";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import {useState} from "react";
-
 import { useSelector } from "react-redux";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
 
+const Navbar = () => {
+    const cartItems = useSelector((store) => store.cart.items);
+    const [loginBtn, setLoginBtn] = useState(true);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State for burger menu
 
-const Navbar=()=>{
-
-    const cartItems=useSelector((store)=>store.cart.items);
-   
-   
-
-    const [loginBtn,setLoginBtn]=useState(true);
-    return(
+    return (
         <div className="navbar">
-            <a href="/"><img src={logo} alt="logo" className="logo"></img></a>
-            <div className="nav-items">
+
+            {/* Logo of the navbar  */}
+            <Link to="/">
+                <img src={logo} alt="logo" className="logo" />
+            </Link>
+
+            <div className={`nav-items ${isMobileMenuOpen ? "open" : ""}`}>
                 <div className="nav-items-list">
-                    <Link className="item" to="/">Home</Link>
-                    <Link className="item" to="/about">About</Link>
-                    <Link className="item" to="/contact">Contact</Link>
-                    <Link className="item" to="/cart">Cart({cartItems.length})</Link>
-                </div>
-
-                <div>
-                    <Link to={loginBtn ? "/login" : "/"}><button className={ loginBtn ? "login-btn" : "logout-btn"} onClick={()=>{
-                        setLoginBtn(!loginBtn);
-
-                    }}>{loginBtn ? "Login" : "Logout"}</button>
+                    <Link className="item" to="/" onClick={() => setIsMobileMenuOpen(false)}>Home</Link>
+                    <Link className="item" to="/about" onClick={() => setIsMobileMenuOpen(false)}>About</Link>
+                    <Link className="item" to="/contact" onClick={() => setIsMobileMenuOpen(false)}>Contact</Link>
+                    <Link className="item" to="/cart" onClick={() => setIsMobileMenuOpen(false)}>
+                        Cart({cartItems.length})
                     </Link>
                 </div>
-                
-                
+                <div>
+                    <Link to={loginBtn ? "/login" : "/"}>
+                        <button
+                            className={loginBtn ? "login-btn" : "logout-btn"}
+                            onClick={() => {
+                                setLoginBtn(!loginBtn);
+                                setIsMobileMenuOpen(false);
+                            }}
+                        >
+                            {loginBtn ? "Login" : "Logout"}
+                        </button>
+                    </Link>
+                </div>
             </div>
-            
-
-
+            {/* Burger menu icon */}
+            <div className="burger-menu" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}>
+                <span className="burger-bar"></span>
+                <span className="burger-bar"></span>
+                <span className="burger-bar"></span>
+            </div>
         </div>
-    )
+    );
+};
 
-}
 export default Navbar;
