543241e6cd1dc2f9af78b59831af15f3d930bdbd
striverz <striverz100x@gmail.com>
2024-10-09 08:09:47 +0530

    contact us section to the application is added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,23 @@
 import React from "react";
 import "./Contact.css"
+import burger from "../../assets/burger.png"
 
 const Contact=()=>{
     return(
-        <div className="contact">Contact</div>
+        <div className="contact">
+            <div className="contact-left">
+                <img src={burger}></img>
+            </div>
+            <form className="contact-right">
+                <h1>Contact US</h1>
+                <input type="text" required></input>
+                <input type="email" required></input>
+                <textarea id="description" name="description" rows="4" placeholder="Enter the description">
+                </textarea>
+                <button>Submit</button>
+
+            </form>
+        </div>
     )
 }
 export default Contact;
